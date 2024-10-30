06e6df80938b70b4ef55ce64e5d5e299034651a7
FelipePy <felipespsampaiomotasp@gmail.com>
2024-10-30 16:40:54 -0300

    alterando UX e UI

--- map.js
+++ map.js
@@ -1,6 +1,7 @@
+
 var map = L.map('map', {
     zoom: 4,
-    center: [-14.2350, -51.9253],
+    center: [-19.2350, -59.9253],
     // Configurações adicionais para suavizar animações
     zoomAnimation: true,
     fadeAnimation: true,
@@ -8,36 +9,56 @@ var map = L.map('map', {
     easeLinearity: 0.25 
 });
 
-function adicionarBanda(bandaNome) {
-    const imageUrl = `imagens/${bandaNome}`; // Caminho da imagem
-    const imageBounds = [[-60.0, -100.0], [30.0, -10.0]]; // Ajuste conforme necessário
-    console.log('Adicionando banda: ', bandaNome);
+// function adicionarBanda(bandaNome) {
+//     const imageUrl = `imagens/${bandaNome}`; // Caminho da imagem
+//     const imageBounds = [[-60.0, -100.0], [30.0, -10.0]]; // Ajuste conforme necessário
     
-    // Adiciona a imagem overlay
-    const overlay = L.imageOverlay(imageUrl, imageBounds).addTo(map);
-    console.log('Overlay adicionado: ', overlay);
+//     // Adiciona a imagem overlay
+//     const overlay = L.imageOverlay(imageUrl, imageBounds).addTo(map);
     
-    // Ajusta o mapa para os limites da imagem
-    map.fitBounds(imageBounds);
-}
+//     // Ajusta o mapa para os limites da imagem
+//     map.fitBounds(imageBounds);
+// }
 
 
 // adicionarBanda('banda1_amazonia.png')
 
-// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//     maxZoom: 6,
-//     minZoom: 4,
-//     initZoom: 4,
-//     attribution: '&copy; OpenStreetMap contributors'
-// }).addTo(map);
-
-L.tileLayer('./tiles/{z}/{x}/{y}.png', {
-    maxZoom: 5, // Ajuste conforme o zoom máximo gerado
-    attribution: 'Tiles &copy; OpenStreetMap contributors'
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 5,
+    minZoom: 4,
+    initZoom: 5,
+    attribution: '&copy; OpenStreetMap contributors',
+    noWrap: true // Impede que os tiles desapareçam
 }).addTo(map);
 
+const tiles1 = L.tileLayer('./tiles/{z}/{x}/{y}.png', {
+    tms: true, // Defina para true se o seu tile estiver no formato TMS
+    opacity: 1,
+    attribution: 'Tiles &copy; OpenStreetMap contributors',
+    maxZoom: 5,
+    minZoom: 4,
+    noWrap: true // Impede que os tiles desapareçam
+}).addTo(map);
+
+const tiles2 = L.tileLayer('./tiles/{z}/{x}/{y}.png', {
+    tms: true, // Defina para true se o seu tile estiver no formato TMS
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    maxZoom: 5,
+    minZoom: 4,
+    opacity: 1,
+    noWrap: true // Impede que os tiles desapareçam
+});
+
+const baseMaps = {
+    "Custom Tiles": tiles1,
+    "OpenStreetMap": tiles2,
+};
+
+// L.control.layers(baseMaps).addTo(map);
+
+
 var southWest = L.latLng(-60.0, -100.0); // Coordenadas do canto sudoeste
-var northEast = L.latLng(30.0, -10.0); // Coordenadas do canto nordeste
+var northEast = L.latLng(30.0, -15.9253); // Coordenadas do canto nordeste
 var bounds = L.latLngBounds(southWest, northEast);
 
 map.setMaxBounds(bounds); // Aplica os limites máximos
@@ -59,7 +80,8 @@ var drawControl = new L.Control.Draw({
     },
     edit: {
         featureGroup: drawnItems // Grupo para armazenar as formas desenhadas
-    }
+    }, 
+    position: 'topright',
 });
 
 map.addControl(drawControl);
